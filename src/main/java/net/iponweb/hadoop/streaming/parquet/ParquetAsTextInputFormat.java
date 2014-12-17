fd0386c2482142a67a96d8dbf8aba5773aaf48f5
Nikita Makeev <whale2.box@gmail.com>
2014-12-17 22:37:40 +0300

    parquet packages updated to 1.5

--- src/main/java/net/iponweb/hadoop/streaming/parquet/ParquetAsTextInputFormat.java
+++ src/main/java/net/iponweb/hadoop/streaming/parquet/ParquetAsTextInputFormat.java
@@ -1,20 +1,4 @@
-/**
- * Copyright 2014 IPONWEB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package net.iponweb.hadoop.streaming.parquet;
+package net.iponweb.hadoop.mr2.lib.parquet;
 
 import com.google.common.base.Joiner;
 import org.apache.hadoop.io.Text;
@@ -220,22 +204,12 @@ public class ParquetAsTextInputFormat<K, V> extends org.apache.hadoop.mapred.Fil
 
     @Override
     public long getLength() throws IOException {
-        try {
-            return realSplit.getLength();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return 0;
+        return realSplit.getLength();
     }
 
     @Override
     public String[] getLocations() throws IOException {
-        try {
-            return realSplit.getLocations();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return realSplit.getLocations();
     }
 
     @Override
